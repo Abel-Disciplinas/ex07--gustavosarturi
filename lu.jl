d27f1e2f66b87d1d50888b2aac29f0dffa4ae342
Gustavo Sarturi <34655704+gustavosarturi@users.noreply.github.com>
2018-10-15 22:31:56 -0300

    Versão final;

--- lu.jl
+++ lu.jl
@@ -1,9 +1,3 @@
-"""`declu(A; ϵ = 1e-12)`
-
-A função `declu` calcula a fatoração LU de uma matriz densa A sem pivoteamento. L e U
-são armazenadas na própria A, com aᵢⱼ = uᵢⱼ se j ≥ i e aᵢⱼ = ℓᵢⱼ se i > j.
-Se algum pivô for menor que ϵ, um erro ocorre.
-"""
 function declu(A, ϵ = 1e-12)
     n = size(A, 1)
     for j = 1:n
@@ -17,38 +11,72 @@ function declu(A, ϵ = 1e-12)
     return A
 end
 
-"""`reslu(A, b)`
-
-Resolve o sistema Ax = b, **substituindo** o b por x.
-"""
 function reslu(A, b)
     n = length(b)
-    
-    for i=1:n
-       for k = 1:i-1
-          b[i] = b[i] - A[i,k] * b[k]
-       end
-    end
-    
-    for i=n:-1:1
-        for j = i+1:n
-            b[i] = b[i] - A[i,j]*b[j]
+
+    for i = 1:n
+        for j = 1:i-1
+            b[i] += - A[i,j] * b[j]
         end
-        b[i] = b[i] / A[i,i]
     end
-    
+
+    for i = n:-1:1
+        for j = i+1:n
+            b[i] += - A[i,j] * b[j]
+        end
+        b[i] *= 1 / A[i,i]
+    end
+
     return b
-    
 end
 
 function reslu2(A, b)
+    n = length(b)
+    for j = 1:n
+        for i = j+1:n
+            b[i] += - A[i,j] * b[j]
+        end
+    end
+
+    for i = n:-1:1
+        for j = i+1:n
+            b[i] += - A[i,j] * b[j]
+        end
+        b[i] *= 1 / A[i,i]
+    end
     return b
 end
 
 function reslu3(A, b)
+    n = length(b)
+    for j = 1:n
+        for i = j+1:n
+            b[i] *= - A[i,j] * b[j]
+        end
+    end
+
+    for j = n:-1:1
+        b[j] *= 1 / A[j,j]
+        for i = j-1:-1:1
+            b[i] += - A[i,j] * b[j]
+        end
+    end
     return b
 end
 
 function reslu4(A, b)
+    n = length(b)
+    for i = 1:n
+        for j = 1:i-1
+            b[i] += - A[i,j] * b[j]
+        end
+    end
+
+    for j = n:-1:1
+        b[j] *= 1 / A[j,j]
+        for i = j-1:-1:1
+            b[i] += - A[i,j] * b[j]
+        end
+    end
     return b
 end
